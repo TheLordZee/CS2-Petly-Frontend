17f6015508b924caa9983b8fddb55d3bb61a11b1
lordzee <l0rclz33@gmail.com>
2021-11-21 02:11:06 -0500

    security

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-const API_KEY = 'vhHjiPtACZUh1MtrZEBjST8zCKwJye9K3eqJKXtG4NkFF1USeE'
-const API_SECRET = 'fyBxCmLyOyATTaoJrk5zwr9AVkPdh1drgETQHux5'
+const API_KEY = 'YOUR KEY HERE'
+const API_SECRET = 'YOUR KEY HERE'
 const PF_BASE_URL = "https://api.petfinder.com/v2/"
-const GOOGLE_KEY = 'AIzaSyD2-I9hSiIzfzM7rcq36JediDgCs-hYFGg'
+const GOOGLE_KEY = 'YOUR KEY HERE'
 
 export {
     API_KEY,
